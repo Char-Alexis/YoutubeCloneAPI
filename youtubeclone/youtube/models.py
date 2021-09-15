064a3740a116edf33a3a2f40836c972b015b4285
charmaine llacuna <charmaine.llacuna@yahoo.com>
2021-09-15 16:32:47 -0400

    Added in extra properties in class components + created a Reply class model

--- youtubeclone/youtube/models.py
+++ youtubeclone/youtube/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class Youtube(models.Model):
-    comment = models.CharField(max_length=500)
+class Comment(models.Model):
+    comment_text = models.CharField(max_length=500)
+    video_id= models.CharField(max_length=500)
+    like= models.IntegerField()
+    dislike= models.IntegerField()
+
+    def __init__(self):
+        return self.name
+
+class Reply(models.Model):
+    reply_text = models.CharField(max_length=500)
+    comment_id = models.ForeignKey(Comment , on_delete=models.CASCADE)
+
+    def __init__(self):
+        return self.name
--- youtubeclone/youtube/views.py
+++ youtubeclone/youtube/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http.response import HttpResponseRedirect
+from .models import Comment, Reply
+
+def create_comment(request):
+    if request.method == "POST":
+        comment_text = request.POST.get('comment_text')
+        video_id = request.video_id
+        
+
 
 # Create your views here.
